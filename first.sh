75b0572d4bb8ad2b1c1328ab26cecca96c5e29d4
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2015-02-22 13:41:29 +0200

    some useful changes
    * changing the swap size via first.sh
    * changing the static interfaces via first.sh
    * changing the resolv.conf to use google DNS
    * changing the hostname

--- first.sh
+++ first.sh
@@ -1,9 +1,30 @@
 !#/bin/bash
 
-echo "## updating the system"
+SWAP_SIZE=512
+STATIC_IP=192.168.1.27
+HOSTNAME=zaherpi2
+
 sudo -s
+echo "## Change the resolve.conf to use google DNS"
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
+echo "## Change the hostname"
+echo $HOSTNAME > /etc/hostname
+/etc/init.d/hostname.sh restart
+
+echo "## Updating the system"
 apt-get update && apt-get -y upgrade
 rpi-update
+
+echo "## Change the swap size"
+echo CONF_SWAPSIZE=$SWAP_SIZE > /etc/dphys-swapfile
+dphys-swapfile setup
+
+echo "## Change the interfaces to static one"
+rm /etc/network/interfaces
+cp $HOME_DIR/interfaces /etc/network/interfaces
+sed -i 's/192.168.1.27 /{$STATIC_IP}/' /etc/network/interfaces
 
 echo "## We are going to reboot now"
 reboot -n
--- nginx.sh
+++ nginx.sh
@@ -2,10 +2,12 @@
 
 NGINX_VERSION=1.6.2
 NGINX_URL=http://nginx.org/download/nginx-{$NGINX_VERSION}.tar.gz
-HOME=/home/pi/my-pi
+HOME_DIR=/home/pi/my-pi
+LOCAL_ADDRESS=zaherpi2.lcoal
+LOCAL_IP=192.168.1.27
 
 
-cd $HOME
+cd $HOME_DIR
 
 echo "## Make sure the language is working"
 sudo locale-gen en_GB.UTF-8
@@ -21,8 +23,8 @@ sudo apt-get install build-essential -y
 sudo apt-get install libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev -y
 
 echo "## Downloading nginx"
-sudo mkdir $HOME/src
-cd $HOME/src
+sudo mkdir $HOME_DIR/src
+cd $HOME_DIR/src
 sudo curl $NGINX_URL -o nginx.tar.gz
 sudo tar zxf nginx.tar.gz
 
@@ -48,16 +50,20 @@ echo "## Installing nginx"
 sudo make && sudo make install
 
 echo "## enable nginx service"
-sudo cp $HOME/nginx_start /etc/init.d/nginx
+sudo cp $HOME_DIR/nginx_start /etc/init.d/nginx
 sudo chmod +x /etc/init.d/nginx
 sudo update-rc.d -f nginx defaults
-#sudo mkdir /etc/nginx/{sites-available,sites-enabled}
-sudo rm -fr $HOME/src
+sudo mkdir /etc/nginx/{sites-available,sites-enabled}
+sudo rm -f /etc/nginx/nginx.conf
+sudo cp $HOME_DIR/nginx.conf /etc/nginx/nginx.conf
+sudo rm -fr $HOME_DIR/src
 
 echo "## create the default site"
-sudo cp $HOME/laravel /etc/nginx/sites-avaliable/laravel
-sudo cd /etc/nginx/sites-enabled
-sudo ln -s ../sites-avaliable/laravel
+sudo cp $HOME_DIR/laravel /etc/nginx/sites-available/laravel
+sudo sed -i 's/zaherpi2.local /{$LOCAL_ADDRESS}/' /etc/nginx/sites-available/laravel
+sudo sed -i 's/192.168.1.27 /{$LOCAL_IP}/' /etc/nginx/sites-available/laravel
+cd /etc/nginx/sites-enabled
+sudo ln -s ../sites-available/laravel
 
 echo "## installing php"
 sudo apt-get update && sudo apt-get -y upgrade
@@ -65,9 +71,9 @@ sudo apt-get -y install php5-fpm php5-mcrypt php5-sqlite sqlite php5-cli php5-xc
 
 
 echo "## Installing composer"
-cd $HOME
+cd $HOME_DIR
 curl -sS http://getcomposer.org/installer | php
-sudo mv $HOME/composer.phar /usr/local/bin/composer
+sudo mv $HOME_DIR/composer.phar /usr/local/bin/composer
 sudo chown pi:pi  /usr/local/bin/composer
 
 echo "## Creating the web directory"
@@ -76,6 +82,7 @@ sudo mkdir www
 sudo chown -R pi:pi www
 cd www
 /usr/local/bin/composer create-project laravel/laravel --prefer-dist
+chown pi:pi  /var/www/laravel
 
 echo "## restarting nginx"
 sudo service nginx restart
