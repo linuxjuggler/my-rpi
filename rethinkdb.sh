69720bf1874708fbd6c22196bfc86276291c5788
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2015-02-23 21:47:01 +0200

    sadly need to work on it manually
    to be able to get it to build v8 by removing the `-m32` flag from this
    file:
    
    external/v8_3.22.24.17/third_party/icu/icu.gyp

--- rethinkdb.sh
+++ rethinkdb.sh
@@ -9,17 +9,38 @@ echo "## Downloading rethinkdb archive"
 mkdir src
 cd src
 wget -c http://download.rethinkdb.com/dist/rethinkdb-$RESTHINKDB_VERSION.tgz
-tar xf rethinkdb-$RESTHINKDB_VERSION.tgz
+tar zxf rethinkdb-$RESTHINKDB_VERSION.tgz
 
-cd rethinkdb-$RESTHINKDB_VERSION
-./configure --with-system-malloc --allow-fetch
 
-echo "## now executing make && make install"
-make && sudo make install
+echo "## sadly need to do everyting manually"
 
-echo "## installing the javascript client"
-npm install rethinkdb
+#cd rethinkdb-$RESTHINKDB_VERSION
+#./configure --with-system-malloc --allow-fetch
 
-echo "## cleanup everything"
-cd ../../
-sudo rm -fr src
+#echo "## now executing make && make install"
+#make && sudo make install
+
+#echo "## installing the javascript client"
+#npm install rethinkdb
+
+#echo "## cleanup everything"
+#cd ../../
+#sudo rm -fr src
+
+#echo "Do you wish to install Laravel Package for rethinkdb ? Yes|No, followed by [ENTER]: "
+
+#read laravel
+
+#if (("$laravel"  == "yes") || ("$laravel"  == "Yes") || ("$laravel"  == "YES") || ("$laravel"  == "y")); then
+
+#    echo "Enter the path for your Laravel installation: ex: /var/www/laravel : "
+#    read laravel_path
+
+#    echo "## installing the driver"
+#    cd $laravel_path
+#    composer require "duxet/laravel-rethinkdb:dev-master"
+#    composer update
+
+#    echo "## back to the user home directory"
+#    cd
+#fi
